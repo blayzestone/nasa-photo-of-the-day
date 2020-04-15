68fb7fc9391f448b7b3cfc67484e197c3f69270c
Blayze <blayzestone@protonmail.com>
2020-04-15 14:59:56 -0700

    feat: display loading until data is recieved

--- src/components/AstronomyImage/AstronomyImage.component.jsx
+++ src/components/AstronomyImage/AstronomyImage.component.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 
 function AstronomyImage({ imageSource, imageAltText }) {
-
-  if (!imageSource) return <h3>Loading...</h3>;
-
   return(
     <img src={imageSource} alt={imageAltText}/>
   );
