266033caa7eb9ef20e1f7a495df7db01386baf80
Blayze <blayzestone@protonmail.com>
2020-04-15 15:58:04 -0700

    feat: date picker that recieves new data
    changing the date will make a new GET request grabbing image of the day
    data for that day.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PageTitle from "./components/PageTitle/PageTitle.component";
 import AstronomyImage from "./components/AstronomyImage/AstronomyImage.component";
 import AstronomyMeta from "./components/AstronomyMeta/AstronomyMeta.component";
 import  AstronomyDesc from "./components/AstronomyDesc/AstronomyDesc.component";
+import DatePicker from "./components/DatePicker/DatePicker.component";
 
 import "./App.css";
 
@@ -12,12 +13,13 @@ const api_key = "6h4EPLYjn8d7acXEfpPBYzfIczSScM8LOAMMDc7N";
 
 function App() {
   const [imageData, setImageData] = useState(null);
+  const [date, setDate] = useState("2020-01-08");
 
   useEffect(() => {
-    axios.get(`${url}?api_key=${api_key}`)
+    axios.get(`${url}?api_key=${api_key}&date=${date}`)
       .then(res => setImageData(res.data))
       .catch(err => console.error(err));
-  }, []);
+  }, [date]);
 
   if (!imageData) return <h3>Loading...</h3>;
 
@@ -27,6 +29,7 @@ function App() {
       <AstronomyImage imageSource={imageData.url} imageAltText={imageData.title}/>
       <AstronomyMeta date={imageData.date} copyright={imageData.copyright} />
       <AstronomyDesc description={imageData.explanation} />
+      <DatePicker date={date} changeHandler={evt => setDate(evt.target.value)}/>
     </div>
   );
 }
