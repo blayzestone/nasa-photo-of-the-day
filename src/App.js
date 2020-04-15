9d50c1639fd8d8492e06b27b4d5a3ec77f7221c1
Blayze <blayzestone@protonmail.com>
2020-04-15 12:56:00 -0700

    chore: GET request to api and set state as data
    import axios and use it to make a GET request to the nasa api. Store
    the data in state.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import "./App.css";
 
+const url = "https://api.nasa.gov/planetary/apod";
+const api_key = "6h4EPLYjn8d7acXEfpPBYzfIczSScM8LOAMMDc7N";
+
 function App() {
+  const [imageData, setImageData] = useState({});
+
+  useEffect(() => {
+    axios.get(`${url}?api_key=${api_key}`)
+      .then(res => setImageData(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <div className="App">
       <p>
